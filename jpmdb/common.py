66e5a78091655043b09042ff130e441d61f63661
Jeff Brennan <jeffbrennan10@gmail.com>
2025-01-28 20:53:38 -0500

    add timeit helper function

---
+++ jpmdb/common.py
@@ -0,0 +1,16 @@
+import time
+from functools import wraps
+
+
+def timeit(func):
+    # https://dev.to/kcdchennai/python-decorator-to-measure-execution-time-54hk
+    @wraps(func)
+    def timeit_wrapper(*args, **kwargs):
+        start_time = time.perf_counter()
+        result = func(*args, **kwargs)
+        end_time = time.perf_counter()
+        total_time = end_time - start_time
+        print(f"Function {func.__name__} Took {total_time:.2f} s")
+        return result
+
+    return timeit_wrapper
