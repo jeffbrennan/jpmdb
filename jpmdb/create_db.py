af1d9a37f1a8dd38a519406f4f3e7a6228f1aecb
Jeff Brennan <jeffbrennan10@gmail.com>
2025-02-08 13:55:39 -0500

    add gold jpmdb dataset

--- jpmdb/create_db.py
+++ jpmdb/create_db.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+
 import duckdb
 
 
@@ -6,16 +7,20 @@ def create_db():
     db_path = Path(__file__).parents[1] / "data" / "jpmdb.duckdb"
     conn = duckdb.connect(str(db_path))
 
-    base_dir = Path(__file__).parents[1] / "data" / "silver"
+    base_dir = Path(__file__).parents[1] / "data"
     tables = [
-        "imdb/title_basics",
-        "imdb/title_ratings",
-        "jpmdb",
+        "silver/imdb/title_basics",
+        "silver/imdb/title_ratings",
+        "silver/jpmdb/jpmdb_cleaned",
+        "silver/jpmdb/stg_jpmdb_combined",
+        "silver/jpmdb/stg_jpmdb_combined",
+        "gold/jpmdb",
     ]
 
     for tbl in tables:
         tbl_name = tbl.split("/")[-1]
         cmd = f"create view if not exists {tbl_name} as select * from delta_scan('{base_dir.as_posix()}/{tbl}')"
+        print(cmd)
         conn.sql(cmd)
 
 
---
+++ jpmdb/gold_jpmdb.py
@@ -0,0 +1,38 @@
+from pathlib import Path
+
+import polars as pl
+
+
+def main():
+    base_path = Path(__file__).parents[1] / "data"
+    jpmdb_silver = pl.read_delta(
+        str(base_path / "silver" / "jpmdb" / "stg_jpmdb_combined")
+    )
+    imdb_data = (
+        pl.read_delta(str(base_path / "silver" / "imdb" / "title_basics"))
+        .join(
+            pl.read_delta(str(base_path / "silver" / "imdb" / "title_ratings")),
+            on="tconst",
+        )
+        .select(
+            "tconst",
+            "primaryTitle",
+            "originalTitle",
+            "titleType",
+            "startYear",
+            "runtimeMinutes",
+            "genres",
+            "averageRating",
+            "numVotes",
+        )
+    )
+
+    combined = jpmdb_silver.join(imdb_data, on="tconst", how="left").drop(
+        "manually_approved", "manually_reviewed_at"
+    ).sort("watched_id")
+
+    combined.write_delta(str(base_path / "gold" / "jpmdb"), mode="overwrite")
+
+
+if __name__ == "__main__":
+    main()
