ce000d52bf22246af25cbcb244df23b5297b0d8d
Jeff Brennan <jeffbrennan10@gmail.com>
2025-02-09 15:59:29 -0500

    switch timeit to display ms

--- jpmdb/common.py
+++ jpmdb/common.py
@@ -9,8 +9,8 @@ def timeit(func):
         start_time = time.perf_counter()
         result = func(*args, **kwargs)
         end_time = time.perf_counter()
-        total_time = end_time - start_time
-        print(f"Function {func.__name__} Took {total_time:.2f} s")
+        total_time = (end_time - start_time) * 1000
+        print(f"Function {func.__name__} Took {total_time:.2f} ms")
         return result
 
     return timeit_wrapper
