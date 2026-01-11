26e316755b2c65909af510ccca4d8752d8e9206c
Jeff Brennan <jeffbrennan10@gmail.com>
2026-01-10 19:38:44 -0500

    ruff

--- jpmdb/pipeline/optimize_tables.py
+++ jpmdb/pipeline/optimize_tables.py
@@ -4,21 +4,25 @@ from deltalake import DeltaTable
 
 
 def main():
-    base_path =  Path(__file__).parents[2] / "data"
+    base_path = Path(__file__).parents[2] / "data"
     all_tables = [
         base_path / "silver" / "imdb" / "title_basics",
         base_path / "silver" / "imdb" / "title_ratings",
         base_path / "silver" / "jpmdb" / "jpmdb_cleaned",
         base_path / "silver" / "jpmdb" / "stg_jpmdb_combined",
-        base_path / "gold" / "jpmdb"
+        base_path / "gold" / "jpmdb",
     ]
 
     for tbl in all_tables:
-        print('-' * 80)
+        print("-" * 80)
         print(tbl)
         dt = DeltaTable(tbl.as_posix())
         print(dt.optimize.compact(target_size=1024 * 1024))
-        print(dt.vacuum(retention_hours=0, enforce_retention_duration=False, dry_run=False))
+        print(
+            dt.vacuum(
+                retention_hours=0, enforce_retention_duration=False, dry_run=False
+            )
+        )
         print(dt.history()[-1])
 
 
